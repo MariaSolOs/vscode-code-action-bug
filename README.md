bb93df059722157b59c0744ca435ab40e2e89fa4
Maria José Solano <majosolano99@gmail.com>
2022-12-18 10:25:24 -0800

    Add instructions

---
+++ README.md
@@ -0,0 +1,9 @@
+# Repro for 
+
+To reproduce the issue, complete the following steps and compare the behavior in VS Code (version 1.74.1, commit 1ad8d514439d5077d2b0b7ee64d2ce82a9308e5a) versus the one in the latest Insiders (version 1.75.0, commit edc432e920c3ec2c2af5e8a99b8e4b782633d298).
+
+1. Clone this repository and run `npm ci`.
+1. Debug the extension with the provided `launch` configuration (just pressing F5 _should work_).
+1. In the experimental instance, open a TypeScript file and request code actions anywhere in the code.
+1. Select the code action titled "Hello, world!".
+1. The expected behavior is that a snackbar saying "Hello, world!" will appear in the lower left corner, which is produced by the command registered with the code action (this happens in VS Code, but not in Insiders).
--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,10 @@ export const activate = (context: vscode.ExtensionContext) => {
 class HelloCodeAction implements vscode.CodeActionProvider {
     provideCodeActions(document: vscode.TextDocument, range: vscode.Range | vscode.Selection, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.ProviderResult<(vscode.CodeAction | vscode.Command)[]> {
         const codeAction = new vscode.CodeAction('Hello, world!');
+        // Removing the edit below "fixes" the issue of the command not being triggered in VS Code Insiders,
+        // but this isn't the best workaround since the docs (https://code.visualstudio.com/api/references/vscode-api#CodeAction)
+        // say that "a CodeAction must set either edit and/or a command. If both are supplied, the edit
+        // is applied first, then the command is executed", but I guess that isn't happening anymore (?).
         codeAction.edit = new vscode.WorkspaceEdit();
         codeAction.command = {
             command: 'sayhello',
